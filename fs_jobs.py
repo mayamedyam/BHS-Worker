c5c1361ff2a95b826a97df01c48c4e885848dcfe
mayamedyam <terziomer1@gmail.com>
2025-08-18 17:38:17 +0300

    Update fs_jobs.py

--- fs_jobs.py
+++ fs_jobs.py
@@ -1,20 +1,24 @@
 import os
 from os import path, mkdir, listdir, remove
 
-
 class fileJobs:
     def __init__(self):
         self.buttonCount = 8
         self.fileLocation = f"{os.getcwd()}/pdf/"
         print(f"[FILE CONTROLLER | MAIN] Completed successfully")
+        
+        # Kurulum sırasında PDF klasörlerini otomatik oluştur
+        self.generateFolders()
 
     def generateFolders(self):
         for file in range(self.buttonCount):
             file += 1
-            if not path.isdir(self.fileLocation + str(file)):
-                mkdir(self.fileLocation + str(file))
+            folder_path = self.fileLocation + str(file)
+            if not path.isdir(folder_path):
+                mkdir(folder_path)
+                print(f"Klasör oluşturuldu: {folder_path}")
 
-    def getFiles(self, folder:  int) -> list:
+    def getFiles(self, folder: int) -> list:
         pathway = self.fileLocation + str(folder) + "/"
         return [os.path.splitext(filename)[0] for filename in os.listdir(pathway)]
 
