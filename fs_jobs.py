15e384b4de2b7601b5c5dbfb92ab71f2fae3edcc
mayamedyam <terziomer1@gmail.com>
2025-08-19 00:34:57 +0300

    Update fs_jobs.py

--- fs_jobs.py
+++ fs_jobs.py
@@ -1,35 +1,31 @@
 import os
-from os import path, mkdir, remove
+from os import path, mkdir, listdir, remove
+
 
 class fileJobs:
     def __init__(self):
         self.buttonCount = 8
-        # Çalışma klasörünün altına devicefolder/pdf/
-        self.baseLocation = path.join(os.getcwd(), "devicefolder")
-        self.fileLocation = path.join(self.baseLocation, "pdf")
-        print(f"[FILE CONTROLLER] Base location: {self.baseLocation}")
-        print(f"[FILE CONTROLLER] PDF location: {self.fileLocation}")
-
-        # Kurulum sırasında klasörleri otomatik oluştur
-        self.generateFolders()
+        self.fileLocation = f"{os.getcwd()}/pdf/"
+        print(f"[FILE CONTROLLER | MAIN] Completed successfully")
 
     def generateFolders(self):
-        # devicefolder ve pdf klasörlerini oluştur
-        if not path.isdir(self.fileLocation):
-            os.makedirs(self.fileLocation)
-            print(f"Ana klasör(ler) oluşturuldu: {self.fileLocation}")
-        else:
-            print("Ana klasör zaten var.")
+        for file in range(self.buttonCount):
+            file += 1
+            if not path.isdir(self.fileLocation + str(file)):
+                mkdir(self.fileLocation + str(file))
 
-        # buttonCount kadar alt klasör oluştur
-        for i in range(1, self.buttonCount + 1):
-            folder_path = path.join(self.fileLocation, str(i))
-            if not path.isdir(folder_path):
-                mkdir(folder_path)
-                print(f"Klasör oluşturuldu: {folder_path}")
-            else:
-                print(f"Klasör zaten var: {folder_path}")
+    def getFiles(self, folder:  int) -> list:
+        pathway = self.fileLocation + str(folder) + "/"
+        return [os.path.splitext(filename)[0] for filename in os.listdir(pathway)]
 
-# Test
-if __name__ == "__main__":
-    controller = fileJobs()
+    def saveFile(self, button: int, fileName: str, content):
+        pathway = self.fileLocation + str(button) + "/"
+        file = open(pathway + fileName + ".pdf", "wb")
+        file.write(content)
+        file.close()
+
+    def deleteFiles(self, button: int, files: list):
+        for file in files:
+            pathway = self.fileLocation + str(button) + "/" + file + '.pdf'
+            if path.exists(pathway):
+                remove(pathway)
